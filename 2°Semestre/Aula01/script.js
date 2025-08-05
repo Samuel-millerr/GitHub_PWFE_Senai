1ece9cb22ae61bbb4d6553e08ef9f5331ba5ea50
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2025-08-05 17:00:02 -0300

    JavaScript

---
+++ 2°Semestre/Aula01/script.js
@@ -0,0 +1,14 @@
+// O JS necessita de uma declaração de variável (var, let, const)
+
+let uma;
+var duas;
+const tres = 10;
+
+uma = 1;
+
+console.log(uma + " " + tres);
+alert("Samuel")
+uma = prompt("Escreva sua idade:")
+console.log(uma)
+uma = confirm("ta certo?")
+console.log(uma)
