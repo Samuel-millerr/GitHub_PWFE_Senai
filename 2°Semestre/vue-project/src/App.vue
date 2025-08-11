ec0c4c053029078a6a8cc47b3b697fae1a41fdd8
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2025-08-11 06:26:35 -0300

    projeto em vue

---
+++ 2°Semestre/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import primeiroComponente from './components/primeiroComponente.vue';
+import dados from './components/dados.vue';
+</script>
+
+<template>
+  <header>VUE</header>
+  <dados/>
+  <primeiroComponente/>
+</template>
+
+<style scoped>
+</style>
+
---
+++ 2°Semestre/vue-project/src/components/dados.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h1>Componente com dados</h1>
+        <p>Nome: Caneta - #1234</p>
+    </div>
+</template>
+
+<style scoped>
+div{
+    text-align: center;
+    border: 2px solid greenyellow;
+    background-color: #1a6509;
+    border-radius: 15px;
+    margin: 30px;
+    padding: 10px;
+}
+</style>
+
